8d6bee1d529d0bc2e105ef7c0610bfcbae9a15aa
jackusay <you@example.com>
2023-05-25 02:15:48 +0800

    modify files

--- Samples/Win7Samples/winui/shell/appshellintegration/ExecuteCommandVerb/ExecuteCommandVerb.cpp
+++ Samples/Win7Samples/winui/shell/appshellintegration/ExecuteCommandVerb/ExecuteCommandVerb.cpp
@@ -27,7 +27,9 @@
 #include "RegisterExtension.h"
 #include <strsafe.h>
 #include <new>  // std::nothrow
-
+#include <atlbase.h>
+#include <functional>
+#include "myuuid.h"
 // Each ExecuteCommand handler needs to have a unique COM object, run UUIDGEN.EXE to
 // create new CLSID values for your handler. These handlers can implement multiple
 // different verbs using the information provided via IInitializeCommand, for example the verb name.
@@ -35,8 +37,9 @@
 // in the property bag.
 
 WCHAR const c_szVerbDisplayName[] = L"ExecuteCommand Verb Sample";
+const int MAX_PATH_CHARS = 3000;//32767 in NTFS, but that's too much
 
-class __declspec(uuid("ffa07888-75bd-471a-b325-59274e73227f"))
+class __declspec(uuid(MYUUID))
     CExecuteCommandVerb : public IExecuteCommand,
                           public IObjectWithSelection,
                           public IInitializeCommand,
@@ -44,8 +47,9 @@ class __declspec(uuid("ffa07888-75bd-471a-b325-59274e73227f"))
                           CAppMessageLoop
 {
 public:
-    CExecuteCommandVerb() : _cRef(1), _psia(NULL), _punkSite(NULL)
+    CExecuteCommandVerb(PWSTR pszCmdLine) : _cRef(1), _psia(NULL), _punkSite(NULL)
     {
+        pszCmdLine_field = pszCmdLine;
     }
 
     HRESULT Run();
@@ -104,15 +108,18 @@ public:
     IFACEMETHODIMP SetNoShowUI(BOOL /* fNoShowUI */)
     { return S_OK; }
 
-    IFACEMETHODIMP SetDirectory(PCWSTR /* pszDirectory */)
-    { return S_OK; }
+    IFACEMETHODIMP SetDirectory(PCWSTR pszDirectory)
+    {
+        wcscpy_s(pszName0, MAX_PATH_CHARS, pszDirectory); 
+        return S_OK; }
 
     IFACEMETHODIMP Execute();
 
     // IObjectWithSelection
     IFACEMETHODIMP SetSelection(IShellItemArray *psia)
     {
-        SetInterface(&_psia, psia);
+        HRESULT hr = SetInterface(&_psia, psia);
+        setSelection_succeeded = SUCCEEDED(hr);
         return S_OK;
     }
 
@@ -146,40 +153,112 @@ public:
 
     void OnAppCallback()
     {
+        WCHAR uuid_wide[50];
+        size_t converted;
+        mbstowcs_s(&converted, uuid_wide, MYUUID, sizeof(MYUUID));
+
+        bool is_debug = pszCmdLine_field[0] == '-' || pszCmdLine_field[0] == 'd';//only "-Embedding" or "d" --> debug
+        if (!setSelection_succeeded) {//directory mode
+            if (is_debug) {
+                MessageBox(NULL, pszName0, uuid_wide, MB_OK | MB_SETFOREGROUND);
+            }
+            else {
+                run_cmd_pipe([this](HANDLE stdIn) {
+                    DWORD written = 0;
+                    WideCharToMultiByte(CP_UTF8, 0, pszName0, -1, mbfile, MAX_PATH_CHARS * 4, NULL, NULL);//to UTF-8
+                    WriteFile(stdIn, mbfile,(DWORD) strlen(mbfile), &written, NULL);
+                    WriteFile(stdIn, L"\n", 1, &written, NULL);//newline
+                });
+            }
+            return;
+        }
         DWORD count;
         _psia->GetCount(&count);
+        CComPtr<IShellItem2> psi;
 
-        IShellItem2 *psi;
-        HRESULT hr = GetItemAt(_psia, 0, IID_PPV_ARGS(&psi));
-        if (SUCCEEDED(hr))
-        {
-            PWSTR pszName;
-            hr = psi->GetDisplayName(SIGDN_PARENTRELATIVEPARSING, &pszName);
-            if (SUCCEEDED(hr))
-            {
-                WCHAR szMsg[128];
-                StringCchPrintf(szMsg, ARRAYSIZE(szMsg), L"%d item(s), first item is named %s", count, pszName);
-                MessageBox(NULL, szMsg, c_szVerbDisplayName, MB_OK | MB_SETFOREGROUND);
-                CoTaskMemFree(pszName);
-            }
-            psi->Release();
+        PWSTR pszName = pszName0;
+        //HRESULT hr ;
+        //HRESULT hr2;
+        if (is_debug) {
+            //debug; show dialog
+            GetItemAt(_psia, 0, IID_PPV_ARGS(&psi));//the first item
+            psi->GetDisplayName(SIGDN_FILESYSPATH, &pszName);
+
+            CComPtr<IShellItem2> psi2;
+            WCHAR* pszName2 = new WCHAR[MAX_PATH_CHARS];
+            GetItemAt(_psia, count - 1, IID_PPV_ARGS(&psi2));//the last item
+            psi2->GetDisplayName(SIGDN_FILESYSPATH, &pszName2);
+
+            WCHAR* szMsg = new WCHAR[MAX_PATH_CHARS * 2 + 400];
+            StringCchPrintf(szMsg, MAX_PATH_CHARS * 2 + 400, L"arg:[%s], %d item(s), first item: [%s], last item: [%s]", pszCmdLine_field, count, pszName, pszName2);
+            MessageBox(NULL, szMsg, uuid_wide, MB_OK | MB_SETFOREGROUND);
+            delete[] pszName2;
+            delete[] szMsg;
+
         }
+        else {//command specified
+            run_cmd_pipe([count, this, &pszName, &psi](HANDLE stdIn) {
+                DWORD written = 0;
+                for (DWORD i = 0; i < count; i++) {
+                    GetItemAt(_psia, i, IID_PPV_ARGS(&psi));
+                    psi->GetDisplayName(SIGDN_FILESYSPATH, &pszName);
+                    WideCharToMultiByte(CP_UTF8, 0, pszName, -1, mbfile, MAX_PATH_CHARS * 4, NULL, NULL);//to UTF-8
+                    WriteFile(stdIn, mbfile, (DWORD)strlen(mbfile), &written, NULL);
+                    WriteFile(stdIn, L"\n", 1, &written, NULL);//newline
+                }
+            });
+
+        }
+    }
+    void run_cmd_pipe(std::function<void(HANDLE)> writer_func) {
+        pszCmdLine_field[wcslen(pszCmdLine_field) - 11] = 0;//ignore " -Embedding"
+
+        //create pipe
+        HANDLE hPipe1[2];
+        HANDLE hChildRead;
+        CreatePipe(&hPipe1[0], &hPipe1[1], NULL, 0);
+        DuplicateHandle(GetCurrentProcess(), hPipe1[0], GetCurrentProcess(), &hChildRead, 0, TRUE, DUPLICATE_SAME_ACCESS);
+        CloseHandle(hPipe1[0]);
+
+        //prepare STARTUPINFO
+        STARTUPINFO si;
+        ZeroMemory(&si, sizeof(si));
+        si.cb = sizeof(STARTUPINFO);
+        si.dwFlags = STARTF_USESTDHANDLES;
+        si.hStdInput = hChildRead;
+        si.hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+        si.hStdError = GetStdHandle(STD_ERROR_HANDLE);
+        //if (pszCmdLine_field[0] == 'h') si.wShowWindow = SW_HIDE; // hide window
+
+        PROCESS_INFORMATION pi;
+        ZeroMemory(&pi, sizeof(pi));
+        //ignore first 2 chars & optionally hide window
+        CreateProcess(NULL, &pszCmdLine_field[2], NULL, NULL, TRUE, (pszCmdLine_field[0] == 'h') ? CREATE_NO_WINDOW : 0, NULL, NULL, &si, &pi);
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+        CloseHandle(hChildRead);
+
+        //write to stdin
+        writer_func(hPipe1[1]);
+        CloseHandle(hPipe1[1]);
     }
 
 private:
     ~CExecuteCommandVerb()
     {
-        SafeRelease(&_psia);
-        SafeRelease(&_punkSite);
     }
-
+    WCHAR* pszCmdLine_field;
     long _cRef;
-    IShellItemArray *_psia;
-    IUnknown *_punkSite;
+    CComPtr<IShellItemArray> _psia;
+    CComPtr<IUnknown> _punkSite;
     HWND _hwnd;
     POINT _pt;
     int _nShow;
     DWORD _grfKeyState;
+
+    CHAR mbfile[MAX_PATH_CHARS * 4];
+    WCHAR pszName0[MAX_PATH_CHARS];
+    bool setSelection_succeeded = false;
 };
 
 // this is called to invoke the verb but this call must not block the caller. to accomidate that
@@ -214,13 +293,34 @@ HRESULT CExecuteCommandVerb::Run()
 
 int APIENTRY wWinMain(HINSTANCE, HINSTANCE, PWSTR pszCmdLine, int)
 {
+
+    BOOL runAsAdmin;
+    {//Check if run as admin
+        SID_IDENTIFIER_AUTHORITY NtAuthority = SECURITY_NT_AUTHORITY;
+        PSID AdministratorsGroup;
+
+        runAsAdmin = AllocateAndInitializeSid(
+            &NtAuthority,
+            2,
+            SECURITY_BUILTIN_DOMAIN_RID,
+            DOMAIN_ALIAS_RID_ADMINS,
+            0, 0, 0, 0, 0, 0,
+            &AdministratorsGroup);
+
+        if (runAsAdmin == TRUE)
+        {
+            if (CheckTokenMembership(NULL, AdministratorsGroup, &runAsAdmin) == FALSE) runAsAdmin = FALSE;
+            FreeSid(AdministratorsGroup);
+        }
+    }
+
     HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
     if (SUCCEEDED(hr))
     {
         DisableComExceptionHandling();
         if (StrStrI(pszCmdLine, L"-Embedding"))
         {
-            CExecuteCommandVerb *pAppDrop = new (std::nothrow) CExecuteCommandVerb();
+            CExecuteCommandVerb* pAppDrop = new (std::nothrow) CExecuteCommandVerb(pszCmdLine);
             if (pAppDrop)
             {
                 pAppDrop->Run();
@@ -230,27 +330,13 @@ int APIENTRY wWinMain(HINSTANCE, HINSTANCE, PWSTR pszCmdLine, int)
         else
         {
             CRegisterExtension re(__uuidof(CExecuteCommandVerb));
-
-            hr = re.RegisterAppAsLocalServer(c_szVerbDisplayName);
-            if (SUCCEEDED(hr))
-            {
-                WCHAR const c_szProgID[] = L"txtfile";
-                WCHAR const c_szVerbName[] = L"ExecuteCommandVerb";
-
-                // register this verb on .txt files ProgID
-                hr = re.RegisterExecuteCommandVerb(c_szProgID, c_szVerbName, c_szVerbDisplayName);
-                if (SUCCEEDED(hr))
-                {
-                    hr = re.RegisterVerbAttribute(c_szProgID, c_szVerbName, L"NeverDefault");
-                    if (SUCCEEDED(hr))
-                    {
-                        MessageBox(NULL,
-                            L"Installed ExecuteCommand Verb Sample for .txt files\n\n"
-                            L"right click on a .txt file and choose 'ExecuteCommand Verb Sample' to see this in action",
-                            c_szVerbDisplayName, MB_OK);
-                    }
-                }
+            if (runAsAdmin && IDYES == MessageBoxA(NULL, "Run as administrator. Install for all users (in HKEY_LOCAL_MACHINE) ?", "ExecuteCommand-Pipe", MB_YESNO)) {
+                re.SetInstallScope(HKEY_LOCAL_MACHINE);
             }
+            hr = re.RegisterAppAsLocalServer(c_szVerbDisplayName);
+            MessageBoxA(NULL,
+                "Installed ExecuteCommand Verb Sample as UUID:{" MYUUID "}",
+                "ExecuteCommand-Pipe", MB_OK);
         }
 
         CoUninitialize();
